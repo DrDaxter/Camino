932f8ff21159bc5e75abced92195bc259c05089b
DrDaxter <guillermoernestoramirezaguilar@gmail.com>
2022-10-26 08:25:00 -0600

    .

--- src/components/HomeButtonsContent.tsx
+++ src/components/HomeButtonsContent.tsx
@@ -1,17 +1,18 @@
-import React, { useCallback, useMemo, useRef } from 'react'
-import BottomSheet from '@gorhom/bottom-sheet';
+import React, { useCallback, useMemo, useRef, useState } from 'react'
 import { Button, StyleSheet, Text, View,Dimensions } from 'react-native';
 import { SimpleButtonIconText } from './SimpleButtonIconText';
 import {PanGestureHandler,GestureHandlerRootView} from 'react-native-gesture-handler';
-import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
-import { constStyles } from '../theme/Const';
+import Animated, { call, runOnJS, useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const HomeButtonsContent = () => {
   const screenWidth = Dimensions.get('screen').width
   const screenHeight = Dimensions.get('screen').height
+  const [iconName, setIconName] = useState("caret-down-outline")
   const startingPosition = 10;
   const pressed = useSharedValue(false);
   const y = useSharedValue(startingPosition);
+  const isDown = useSharedValue("NO")
 
   const eventHandler = useAnimatedGestureHandler({
     onStart: (event, ctx) => {
@@ -24,13 +25,20 @@ export const HomeButtonsContent = () => {
       console.log(y.value)
     },
     onEnd: (event, ctx) => {
+      runOnJS(ejemplo)()
       if(y.value < (screenHeight/3) && y.value > 1){
         y.value = (screenWidth /3)
+        isDown.value = "SI"
       }else if(y.value > 1 && y.value >= 0){
         y.value = withSpring(startingPosition)
+        isDown.value = "NO"
       }
     },
   });
+  
+  const ejemplo = () => {
+    console.log("HOLAAA")
+  }
 
   const uas = useAnimatedStyle(() => {
     return {
@@ -46,6 +54,14 @@ export const HomeButtonsContent = () => {
     }}>
       <PanGestureHandler onGestureEvent={eventHandler}>
         <Animated.View style={[{...styles.helOptionsContent,width:screenWidth},uas]} >
+          <View style={styles.topElementsContent}>
+           {/*  <Icon 
+              name={iconName}
+              color="#000"
+              size={20}
+            /> */}
+            <Text style={{color:"#000"}}>{iconName}</Text>
+          </View>
           <View style={styles.buttonsContainer}>
             <SimpleButtonIconText 
               text={"Servicio \nmecánico"}
@@ -67,6 +83,9 @@ export const HomeButtonsContent = () => {
 }
 
 const styles = StyleSheet.create({
+  topElementsContent:{
+    alignItems:'center'
+  },
   helOptionsContent:{
     position:'absolute',
     bottom:0,
