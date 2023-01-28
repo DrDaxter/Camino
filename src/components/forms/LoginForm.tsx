ea905ecc264aa2f524e5ec3a0c0f9091202de989
guillermo.ramirez <guillermo.ramirez@abank.com.sv>
2023-01-27 21:01:11 -0600

    se agrego custom input con formik validaciones

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
 import { Colors } from '../../theme/Colors'
+import { InputLable } from '../../utils/InputLable'
 
 export type formProps = {
     email:'email',
@@ -8,15 +11,40 @@ export type formProps = {
 }
 export const LoginForm = () => {
 
+    const signupSchema = Yup.object().shape({
+        email: Yup.string().required('Required'),
+        password: Yup.string().max(10,"too long").required('Required')
+    })
   return (
     <ScrollView style={styles.formContent}>
-        <View style={styles.inputContentGeneral}>
-
-        </View>
-        
-        <View style={styles.inputContentGeneral}>
-            
-        </View>
+        <Formik
+            initialValues={{email:'',password:''}}
+            onSubmit={({email,password}) => {
+                console.log(email,password)
+            }}
+            validationSchema={signupSchema}
+        >
+        {({
+            handleChange,
+            handleSubmit,
+            handleBlur,
+            values,
+            isValid,
+            touched,
+            errors,
+            resetForm,
+        }) => (
+            <View style={styles.inputContentGeneral}>
+                <InputLable 
+                    label="Email"
+                    onValueChange={handleChange('email')}
+                    onBlurChange={handleBlur('email')}
+                    value={values.email}
+                />
+                {errors.email && touched.email && <Text style={{color:"red"}}>{errors.email}</Text>}
+            </View>
+        )}
+        </Formik>
     </ScrollView>
   )
 }
--- src/utils/InputLable.tsx
+++ src/utils/InputLable.tsx
@@ -1,21 +1,20 @@
 import React,{useState,useRef,useEffect} from 'react'
 import {StyleSheet,View,TextInput,Text,Animated, Easing} from 'react-native'
 import { Colors } from '../theme/Colors'
-import {Control, Controller, useForm} from 'react-hook-form';
 import { formProps } from '../components/forms/LoginForm';
 
 interface Props {
-    control: Control<formProps, any>,
-    name:string,
     label:string,
-    rules: {},
+    value:string,
+    onValueChange: (value: string) => void
+    onBlurChange: (e: FocusEvent) => void
 }
 
 export const InputLable = ({
-    control,
-    name,
-    label,
-    rules = {},
+   label,
+   value,
+   onValueChange,
+   onBlurChange
 }:Props) => {
 
     const [isFocused, setIsFocused] = useState(false)
@@ -26,7 +25,7 @@ export const InputLable = ({
       Animated.timing(
         focusAnim,
         {
-            toValue: isFocused || !!text ? 1 : 0,
+            toValue: isFocused || !!value ? 1 : 0,
             duration:150,
             useNativeDriver:true,
             easing: Easing.bezier(0.4,0,0.2,1)
@@ -35,63 +34,50 @@ export const InputLable = ({
     }, [focusAnim,isFocused])
     
   return (
+
     <View>
-        <Controller 
-            control={control}
-            rules={{
-                maxLength:10,
-                required:true
-               }}
-            render={({field:{onChange,onBlur,value}, fieldState: {error}})=>(
-                <View>
-                    <TextInput 
-                        onBlur={onBlur}
-                        onFocus={(event) => {
-                            setIsFocused(true)
-                        }}
-                        style={[styles.input]} 
-                        onChangeText={onChange}
-                        value={value}
-                    />
-                    <Animated.View 
-                        style={{
-                        ...styles.labelContainer, 
-                        transform:[
-                            {
-                                scale:focusAnim.interpolate({
-                                    inputRange:[0,1],
-                                    outputRange:[1,0.75]
-                                })
-                            },
-                            {
-                                translateY: focusAnim.interpolate({
-                                    inputRange:[0,1],
-                                    outputRange:[24,-12]
-                                })
-                            },
-                            {
-                                translateX:focusAnim.interpolate({
-                                    inputRange:[0,1],
-                                    outputRange:[16,0]
-                                })
-                            }
-                        ]
-                        }}
-                    >
-                        <Animated.Text style={{
-                            ...styles.label, 
-                            color: isFocused ? '#080F9C' : '#B9C4CA'
-                            }}>
-                            {label}
-                        </Animated.Text>
-                    </Animated.View>
-                    {error && (
-                        <Text style={{color: 'red', alignSelf: 'stretch'}}>{error.message || 'Error'}</Text>
-                    )}
-                </View>
-            )}
-            name="email"
+        <TextInput 
+        style={styles.input}
+        onChangeText={onValueChange}
+            onFocus={() => setIsFocused(true)}
+            onBlur={(e) => {
+                onBlurChange?.(e)
+                setIsFocused(false)
+            }}
+            value={value}
         />
+        <Animated.View 
+            style={{
+            ...styles.labelContainer, 
+            transform:[
+                {
+                    scale:focusAnim.interpolate({
+                        inputRange:[0,1],
+                        outputRange:[1,0.75]
+                    })
+                },
+                {
+                    translateY: focusAnim.interpolate({
+                        inputRange:[0,1],
+                        outputRange:[24,-12]
+                    })
+                },
+                {
+                    translateX:focusAnim.interpolate({
+                        inputRange:[0,1],
+                        outputRange:[16,0]
+                    })
+                }
+            ]
+            }}
+        >
+            <Animated.Text style={{
+                ...styles.label, 
+                color: isFocused ? '#080F9C' : '#B9C4CA'
+                }}>
+                {label}
+            </Animated.Text>
+        </Animated.View>
     </View>
   )
 }
